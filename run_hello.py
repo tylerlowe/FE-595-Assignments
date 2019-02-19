4b927176f9986c7ada26d82f1c9724a3f82a828b
rameshrallapalli <44247646+rameshrallapalli@users.noreply.github.com>
2019-02-18 22:12:53 -0500

    Add files via upload

---
+++ run_hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    print("what should I print?")
+    return "Hello, world!"
+
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello_person(name):
+    return "Hello, {}!".format(name)
+
+
+if __name__ == "__main__":
+app.run(host='0.0.0.0', port=8080)
